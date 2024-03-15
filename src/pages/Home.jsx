1a1fa0f816c2328ddd72b372579356f46ab807f9
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2024-03-15 14:25:50 +0500

    loading is added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,27 +5,40 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
-  const [searchProducts, setSearchProducts] = useState(products);
-  const [filter, setFilterProducts] = useState("All");
+  const [searchProducts, setSearchProducts] = useState([]);
+  const [filter, setFilter] = useState("All");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const pageSize = 6;
+
   const fetchProducts = async () => {
+    setLoading(true);
     try {
       const res = await axios.get("https://dummyjson.com/products");
       const data = await res.data.products;
+      setTotalPages(Math.ceil(res.data.totalCount / pageSize));
       setProducts(data);
     } catch (error) {
-      console.log("hatolik bor", error);
+      console.log("Error occurred while fetching products:", error);
+    } finally {
+      setLoading(false);
     }
+  };
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [currentPage]);
+
   useEffect(() => {
     setSearchProducts(products);
   }, [products]);
 
   const handleSearch = (e) => {
-    e.preventDefault();
     const text = e.target.value.trim().toLowerCase();
     if (text) {
       setSearchProducts(
@@ -39,16 +52,17 @@ const Home = () => {
       setSearchProducts(products);
     }
   };
+
   const handleFilter = (e) => {
-    setFilterProducts(e.target.value)
-    const filter = e.target.value;
-    if(filter==='All'){
-      setSearchProducts(products)
-    }else{
-      setSearchProducts(products.filter(product=>product.category===filter))
+    const selectedFilter = e.target.value;
+    setFilter(selectedFilter);
+    if (selectedFilter === "All") {
+      setSearchProducts(products);
+    } else {
+      setSearchProducts(
+        products.filter((product) => product.category === selectedFilter)
+      );
     }
-  
-    
   };
 
   return (
@@ -59,7 +73,7 @@ const Home = () => {
           <input
             type="search"
             onChange={handleSearch}
-            placeholder="Searching ..."
+            placeholder="Search..."
           />
           <select
             name="filter"
@@ -76,24 +90,48 @@ const Home = () => {
           </select>
         </div>
         <div className="product_content">
-          {searchProducts.length > 0 &&
-            searchProducts.map((product) => {
-              console.log(product);
-              return (
-                <div className="product_card" key={product.id}>
-                  <img src={product.thumbnail} alt={product.name} />
-
-                  <div className="product_body">
-                    <h2>{product.category}</h2>
-                    <span>{product.brand}</span>
-                    <p>${product.price}</p>
-                    <Link to={`/product/${product.id}`}>
-                      <button className="product_btn">see more</button>
-                    </Link>
-                  </div>
+          {loading ? (
+            <div className="loading">
+              <div className="   loadingio-spinner-spinner-6198pfzjivt">
+                <div className="ldio-g5sxjzkryea">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
                 </div>
-              );
-            })}
+              </div>
+            </div>
+          ) : (
+            searchProducts.length > 0 &&
+            searchProducts.map((product) => (
+              <div className="product_card" key={product.id}>
+                <img src={product.thumbnail} alt={product.name} />
+                <div className="product_body">
+                  <h2>{product.category}</h2>
+                  <span>{product.brand}</span>
+                  <p>${product.price}</p>
+                  <Link to={`/product/${product.id}`}>
+                    <button className="product_btn">See more</button>
+                  </Link>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+        <div className="pagination">
+          {Array.from({ length: totalPages }, (_, i) => (
+            <button key={i} onClick={() => handlePageChange(i + 1)}>
+              {i + 1}
+            </button>
+          ))}
         </div>
       </div>
     </div>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -6,17 +6,21 @@ import { Link } from "react-router-dom";
 import "./Product.scss";
 
 const Product = () => {
+  const [loading, setLoading] = useState(false);
   const [product, setProduct] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
     const fetchProduct = async () => {
+      setLoading(true);
       try {
         const res = await axios.get(`https://dummyjson.com/products/${id}`);
         const data = res.data;
         setProduct(data);
       } catch (error) {
         console.log("Error occurred:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -27,26 +31,47 @@ const Product = () => {
     <div className="product">
       <div className="container">
         <div className="pro_head">
-    <Link to='../home'  className="btn_back">
-        go back
-    </Link>
-        <h1>Details of Product</h1>
+          <Link to="../home" className="btn_back">
+            go back
+          </Link>
+          <h1>Details of Product</h1>
         </div>
 
         <div className="pro_content">
-          {product && (
-            <div className="pro_card">
-              <div className="pro_img">
-                <img src={product.images[1]} alt={product.name} />
-                <span>-{product.discountPercentage}$</span>
-              </div>
-              <div className="pro_texts">
-                <h3>{product.title}</h3>
-                <h1>{product.category}</h1>
-                <span>{product.brand}</span>
-                <p>{product.description}</p>
+          {loading ? (
+            <div className="loading">
+              <div className="   loadingio-spinner-spinner-6198pfzjivt">
+                <div className="ldio-g5sxjzkryea">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
               </div>
             </div>
+          ) : (
+            product && (
+              <div className="pro_card">
+                <div className="pro_img">
+                  <img src={product.images[1]} alt={product.name} />
+                  <span>-{product.discountPercentage}$</span>
+                </div>
+                <div className="pro_texts">
+                  <h3>{product.title}</h3>
+                  <h1>{product.category}</h1>
+                  <span>{product.brand}</span>
+                  <p>{product.description}</p>
+                </div>
+              </div>
+            )
           )}
         </div>
       </div>
